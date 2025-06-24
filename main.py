b5e40682dc788c6101a7a46217fe18b40e623c03
British-Library-Technical-Services <tomisalive@gmail.com>
2025-06-24 15:21:07 +0100

    Reformatted layout

--- main.py
+++ main.py
@@ -13,8 +13,23 @@ split_file = "test_ouput_1.mp4"
 smpte_tc_in = "00:01:01:24"
 smpte_tc_out = "00:01:25:22"
 
+
 def timecode_split(input_file, tc_in, tc_out, output_file):
-    subprocess.call(["ffmpeg", "-i", input_file, "-ss", tc_in, "-to", tc_out, "-c", "copy", output_file]) 
+    subprocess.call(
+        [
+            "ffmpeg",
+            "-i",
+            input_file,
+            "-ss",
+            tc_in,
+            "-to",
+            tc_out,
+            "-c",
+            "copy",
+            output_file,
+        ]
+    )
+
 
 def frames_to_ms(smpte_tc) -> int:
     separator = ":"
@@ -22,13 +37,13 @@ def frames_to_ms(smpte_tc) -> int:
     ff = int(tc_array.pop())
     ms = int((ff / 25) * 1000)
     tc_array.append(str(ms))
-    hh_mm_ss_ms = separator.join(tc_array) 
+    hh_mm_ss_ms = separator.join(tc_array)
     print(hh_mm_ss_ms)
-    
+
     return ms
-    
-ms_tc_in = frames_to_ms(smpte_tc_in) 
+
+
+ms_tc_in = frames_to_ms(smpte_tc_in)
 ms_tc_out = frames_to_ms(smpte_tc_out)
+
 # timecode_split(source_file, smpte_tc_in, smpte_tc_out, split_file)
-
-
